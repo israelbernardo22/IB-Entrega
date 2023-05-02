da2f06e999da70e6e71b6af0a14b430ddc7203dc
israelbernardo22 <ib94534@unifei.edu.br>
2023-05-02 11:36:20 -0300

    comentarios js

--- codigo.js
+++ codigo.js
@@ -3,6 +3,7 @@
 
 console.log("teste do js");
 
+//função com todo o calculo elaborado
 function calculaentrega() {
     let distancia, tempo, seguro, manutencao, oleo, gasolina, lucro, custov, custotempo, total;
     distancia = parseFloat(document.getElementById("edtKM").value)
@@ -11,23 +12,23 @@ function calculaentrega() {
     seguro = 0.50;
     manutencao = 0.05 * distancia;
     oleo = (0.022 * distancia) * 2;
-    gasolina = (0.193 * distancia) * 2;
-    lucro = 0.50 * distancia;
-    custov = (distancia * 0.022) + (distancia * 0.193);
-    if (distancia > 15){
+    gasolina = (0.193 * distancia) * 2; //custo de gasolina com base na distancia indicada pelo usuario
+    lucro = 0.50 * distancia; //Lucro com base na distancia
+    custov = (distancia * 0.022) + (distancia * 0.193); //custo da volta levando em conta apenas o gasto de volta da corrida
+    if (distancia > 15){ //condições que aumenta o lucro do entregador com base na distancia
         lucro = 5 + (distancia * 0.75);
     } if (distancia > 22){
         lucro = 7 +(distancia * 0.65);
     } if (distancia >= 50){
         lucro = 10 + (distancia * 0.55);
-    } if (tempo >= 20){
+    } if (tempo >= 20){ //condições que aumenta o lucro do entregador com base no tempo gasto
         custotempo = 2 + (tempo * 0.17);
     } else if (tempo < 20) {
         custotempo = 2;
     }
 
-    total = seguro + manutencao + oleo + gasolina + lucro + custov + custotempo;
+    total = seguro + manutencao + oleo + gasolina + lucro + custov + custotempo; //somando todos os valores inclusos na corrida
 
     document.getElementById("resp").innerText = "Com " + distancia +
-     " km e " + tempo + " minutos, o valor da corrida será: " + total.toFixed(2);
+     " km e " + tempo + " minutos, o valor da corrida será: " + total.toFixed(2); //imprimindo o resultado
 }
